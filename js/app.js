379dd9639e6bba4c10f841fd66a4603884c7c01d
mdtanvirisl <mdtanvirislam945@gmail.com>
2023-07-27 11:34:32 +0600

    Small change To input field

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const playerField = getElement('player-field');
     const playerFieldValue = parseFloat(playerField.value);
 
+    playerField.value = "";
     const totalPlayerCost = playerFieldValue * 5;
 
     const playerExpenses = getElement('player-expenses');
@@ -16,9 +17,11 @@ document.getElementById('btn-total').addEventListener('click', function () {
 
     const managerField = getElement('manager-field');
     const managerFieldValue = parseFloat(managerField.value);
+    managerField.value = "";
 
     const coachField = getElement('coach-field');
     const coachFieldValue = parseFloat(coachField.value);
+    coachField.value = "";
 
     const totalCost = playerExpensesValue + managerFieldValue + coachFieldValue;
 
